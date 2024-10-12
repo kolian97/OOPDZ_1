7d76e38e7b804fa5e628430053ed775947152ef1
kolian97 <kolian_97@mail.ru>
2024-10-12 14:40:36 +0300

    Добавьте файлы проекта.

---
+++ FamalyMembers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDZ_1
+{
+    public class FamalyMembers
+    {
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public string FullName { get; set; }
+        public FamalyMembers Mother { get; set; }
+        public FamalyMembers Father { get; set; }
+        public FamalyMembers[] GetGrandMotherName()
+        {
+            return new FamalyMembers[] { Mother?.Mother, Father?.Mother };
+        }
+        public FamalyMembers[] GetGrandFatherName()
+        {
+            return new FamalyMembers[] { Father?.Father, Mother?.Father };
+        }
+        public FamalyMembers[] GetParentsName()
+        {
+            return new FamalyMembers[] { Father, Mother };
+        }
+
+    }
+    public enum Gender { man, woman }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+
+namespace OOPDZ_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FamalyMembers GrandFatherone = new FamalyMembers()
+            {
+                FullName = "Кирил",
+                Age = 40,
+                Gender = Gender.man
+            };
+            FamalyMembers GrandMotherone = new FamalyMembers()
+            {
+                FullName = "Клара",
+                Age = 41,
+                Gender = Gender.woman
+            };
+            FamalyMembers GrandFathersecond = new FamalyMembers()
+            {
+                FullName = "Никита",
+                Age = 60,
+                Gender = Gender.man
+            };
+            FamalyMembers GrandMothersecond = new FamalyMembers()
+            {
+                FullName = "Маша",
+                Age = 61,
+                Gender = Gender.woman
+            };
+            FamalyMembers Father = new FamalyMembers()
+            {
+                FullName = "Тимофей",
+                Age = 30,
+                Gender = Gender.man,
+                Father = GrandFatherone,
+                Mother = GrandMotherone
+            };
+            FamalyMembers Mother = new FamalyMembers()
+            {
+                FullName = "Наташа",
+                Age = 27,
+                Gender = Gender.woman,
+                Father = GrandFathersecond,
+                Mother = GrandMothersecond
+            };
+            FamalyMembers Son = new FamalyMembers()
+            {
+                FullName = "Виктор",
+                Age = 9,
+                Gender = Gender.man,
+                Father = Father,
+                Mother = Mother
+            };
+            FamalyMembers daugher = new FamalyMembers()
+            {
+                FullName = "Алиса",
+                Age = 8,
+                Gender = Gender.woman,
+                Father = Father,
+                Mother = Mother
+            };
+            var GrandMothers = Son.GetGrandMotherName();
+            var GrandFathers = Son.GetGrandFatherName();
+            var Parents = Son.GetParentsName();
+            Console.WriteLine(GrandMothers[0]?.FullName);
+            Console.WriteLine(GrandMothers[1]?.FullName);
+            Console.WriteLine(GrandFathers[0]?.FullName);
+            Console.WriteLine(GrandFathers[1]?.FullName);
+            Console.WriteLine(Parents[0]?.FullName);
+            Console.WriteLine(Parents[1]?.FullName);
+        }
+    }
+}
